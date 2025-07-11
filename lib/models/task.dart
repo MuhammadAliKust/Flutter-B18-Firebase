19953ca593ce1457ef3659b625ea941a960ea655
Muhammad Ali <kustalics@gmail.com>
2025-07-11 12:06:51 +0500

    First Commit

---
+++ lib/models/task.dart
@@ -0,0 +1,45 @@
+// To parse this JSON data, do
+//
+//     final taskModel = taskModelFromJson(jsonString);
+
+import 'dart:convert';
+
+class TaskModel {
+  final String? docId;
+  final String? title;
+  final String? description;
+  final String? image;
+  final String? priorityID;
+  final bool? isCompleted;
+  final int? createdAt;
+
+  TaskModel({
+    this.docId,
+    this.title,
+    this.description,
+    this.priorityID,
+    this.image,
+    this.isCompleted,
+    this.createdAt,
+  });
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) => TaskModel(
+    docId: json["docID"],
+    title: json["title"],
+    description: json["description"],
+    image: json["image"],
+    isCompleted: json["isCompleted"],
+    priorityID: json["priorityID"],
+    createdAt: json["createdAt"],
+  );
+
+  Map<String, dynamic> toJson(String taskID) => {
+    "docID": taskID,
+    "title": title,
+    "description": description,
+    "image": image,
+    "priorityID": priorityID,
+    "isCompleted": isCompleted,
+    "createdAt": createdAt,
+  };
+}
---
+++ lib/services/task.dart
@@ -0,0 +1,92 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../models/task.dart';
+
+
+class TaskServices {
+  ///Create Task
+  Future createTask(TaskModel model) async {
+    DocumentReference docRef = FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc();
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(docRef.id)
+        .set(model.toJson(docRef.id));
+  }
+
+  ///Update Task
+  Future updateTask(TaskModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(model.docId)
+        .update({"title": model.title, "description": model.description});
+  }
+
+  ///Delete Task
+  Future deleteTask(String taskID) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(taskID)
+        .delete();
+  }
+
+  ///Mark Task as Complete
+  Future markTaskAsComplete(String taskID) async {
+    return await FirebaseFirestore.instance
+        .collection('taskCollection')
+        .doc(taskID)
+        .update({'isCompleted': true});
+  }
+
+  ///Get All Task
+  Stream<List<TaskModel>> getAllTask() {
+    return FirebaseFirestore.instance
+        .collection('taskCollection')
+        .snapshots()
+        .map(
+          (taskList) => taskList.docs
+              .map((taskJson) => TaskModel.fromJson(taskJson.data()))
+              .toList(),
+        );
+  }
+
+  ///Get Completed Task
+  Stream<List<TaskModel>> getCompletedTask() {
+    return FirebaseFirestore.instance
+        .collection('taskCollection')
+        .where('isCompleted', isEqualTo: true)
+        .snapshots()
+        .map(
+          (taskList) => taskList.docs
+              .map((taskJson) => TaskModel.fromJson(taskJson.data()))
+              .toList(),
+        );
+  }
+
+  ///Get InCompleted Task
+  Stream<List<TaskModel>> getInCompletedTask() {
+    return FirebaseFirestore.instance
+        .collection('taskCollection')
+        .where('isCompleted', isEqualTo: false)
+        .snapshots()
+        .map(
+          (taskList) => taskList.docs
+              .map((taskJson) => TaskModel.fromJson(taskJson.data()))
+              .toList(),
+        );
+  }
+
+  ///Get Priority Task
+  Stream<List<TaskModel>> getPriorityTask(String priorityID) {
+    return FirebaseFirestore.instance
+        .collection('taskCollection')
+        .where('priorityID', isEqualTo: priorityID)
+        .snapshots()
+        .map(
+          (taskList) => taskList.docs
+              .map((taskJson) => TaskModel.fromJson(taskJson.data()))
+              .toList(),
+        );
+  }
+}
